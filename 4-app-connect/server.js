aa6333b3d491fc3ff138133332b88c3ee9731d93
JAreina <jareinafdez@gmail.com>
2018-01-02 18:22:47 +0100

    modulo connect

--- 4-app-connect/server.js
+++ 4-app-connect/server.js
@@ -1,6 +1,20 @@
 const connect = require('connect');
 const app = connect();
 
+
+/* middlewares*/
+
+function hola(req,res,next){
+	res.setHeader('Content-type','text/plain')
+	res.write("HOLA");
+	res.end();
+}
+
+app.use(hola);
+
+
+
+
 app.listen(3001);
 
 console.log("servidor puerto 3001")
