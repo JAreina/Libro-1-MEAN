73f907d5a4d393e0c5906f2b2580ebbfb81e110a
JAreina <jareinafdez@gmail.com>
2018-01-03 09:24:18 +0100

    modulo connect

--- 4-app-connect/server.js
+++ 4-app-connect/server.js
@@ -3,6 +3,12 @@ const app = connect();
 
 
 /* middlewares*/
+function logger ( req,res,next){
+	console.log(req.method,req.url);
+	next();
+}
+
+
 
 function hola(req,res,next){
 	res.setHeader('Content-type','text/plain')
@@ -10,8 +16,22 @@ function hola(req,res,next){
 	res.end();
 }
 
-app.use(hola);
+function adios(req,res,next){
+	res.setHeader('Content-type','text/plain');
+	res.write("ADIOS");
+	res.end();
+}
 
+
+
+
+app.use(logger);
+//app.use(hola);
+
+//rutas
+app.use('/',logger)
+app.use('/hola',hola);
+app.use('/adios',adios);
 
 
 
