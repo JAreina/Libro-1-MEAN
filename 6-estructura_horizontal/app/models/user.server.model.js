c9c4ff47a88385d7b43f06c7e6e5c66c311f6368
JAreina <jareinafdez@gmail.com>
2018-01-05 10:49:52 +0100

    atributos virtuales en el esquema mongoose

--- 6-estructura_horizontal/app/models/user.server.model.js
+++ 6-estructura_horizontal/app/models/user.server.model.js
@@ -8,7 +8,56 @@ const UserSchema = new Schema({
   nombre: String,
   apellido: String,
   email: String,
-  nombreUsuario: String,
-  password: String
+  nombreUsuario: {
+    type: String,
+    trim: true
+  },
+  password: String,
+  created:{
+    type: Date,
+    default: Date.now
+  },
+  website: {
+          type: String,
+          set: function(url) {
+                  if (!url) {
+                  return url;
+                  } else {
+                    if (url.indexOf('http://') !== 0 &&
+                    url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                    }
+                    return url;
+                  }
+          }
+},
+website: {
+          type: String,
+          get: function(url) {
+                if (!url) {
+                return url;
+                } else {
+                    if (url.indexOf('http://') !== 0 &&
+                    url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                    }
+                    return url;
+                }
+            }
+}
 })
+
+
+// atributos virtuales
+
+UserSchema.virtual('fullName').get(function(){
+return this.nombre + ' ' + this.apellido;
+}).set(function(fullName){
+  const splitName = fullName.split(' ');
+  this.nombre = splitName[0] || ' ';
+  this.apellido = splitName[1] || ' ';
+})
+
+
+UserSchema.set('toJSON',{getter: true, virtuals: true})
 mongoose.model('Usuario',UserSchema);
